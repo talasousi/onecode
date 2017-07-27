5030e99e17389176a6f50fd80a0a756c2087a0b1
talasousi <talasousi@me.com>
2017-07-27 17:55:56 +0300

    updated side project

--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,19 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Post(models.Model):
-	title = models.CharField(max_length=255)
+	title = models.CharField(max_length=50)
+	image = models.ImageField(upload_to="blog_images", null=True, blank=True)
 	content = models.TextField()
-	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
-	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	timestamp = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return self.title
-		
+
+	def get_absolute_url(self):
+		return reverse("posts:detail", kwargs={"post_id": self.id})
+
+	class Meta:
+		ordering = ['-timestamp', '-updated']
--- posts/views.py
+++ posts/views.py
@@ -1,35 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-import random
 from .models import Post
 from django.shortcuts import get_object_or_404
-
+from .forms import PostForm
+from django.contrib	import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
-def post_create(request):
-	post_list = Post.objects.all()
-	content = Post.objects.all().first()
+
+#def post_create(request):
+#	content = Post.objects.all().first()
+#	context = {
+#		"title": "Post Page",
+#		"content": content,
+##		"list": post_list,
+#	}
+#	return render(request, 'create.html', context)
+
+def post_update(request, post_id):
+	post_object = get_object_or_404(Post, id=post_id)
+	form = PostForm(request.POST or None, request.FILES or None, instance=post_object)
+	if form.is_valid():
+		form.save()
+		messages.success(request, "Post updated")
+		return redirect("posts:list")
 	context = {
-		"title": "Post Page",
-		"content": content,
-		"random_numbers": random.randint(1,1000),
-		
-		"list": post_list,
+		"form": form,
+		"post_object": post_object,
 	}
-	return render(request, 'create.html', context)
 
-def post_update(request):
-	return HttpResponse("<h1> Update <h1>")
+	return render(request, 'post_update.html', context)
 
-def post_delete(request):
-	return HttpResponse("<h1> Delete <h1>")
+def post_delete(request, post_id):
+	Post.objects.get(id=post_id).delete()
+	messages.warning(request, "message deleted")
+	return redirect("posts:list")
 
 def post_list(request):
-	obj_list = Post.objects.all()
-	context = {
-		"post_list": obj_list,
-	}
+    obj_list = Post.objects.all()
 
-	return render(request, 'post_list.html', context)
+    paginator = Paginator(obj_list, 4) # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        objs = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        objs = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        objs = paginator.page(paginator.num_pages)
+
+    context = {
+    "post_list": objs,
+    }
+
+    return render(request, 'post_list.html', context)
 
 def post_detail(request, post_id):
 	obj = get_object_or_404(Post, id=post_id)
@@ -37,3 +62,15 @@ def post_detail(request, post_id):
 		"instance": obj,
 	}
 	return render(request, 'post_detail.html', context)
+
+def post_create(request):
+	form = PostForm(request.POST or None, request.FILES or None)
+	if form.is_valid():
+		form.save()
+		messages.success(request, "Post created")
+		return redirect("posts:list")
+	context = {
+		"form": form,
+	}
+
+	return render(request, 'post_create.html', context)
