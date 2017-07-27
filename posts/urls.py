f708b5801bb33dadc10445bc2b10e1e1a4c65e87
talasousi <talasousi@me.com>
2017-07-27 18:03:37 +0300

    updated again

--- posts/urls.py
+++ posts/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     
     url(r'^create/$', views.post_create , name='create'),
-    url(r'^update/$', views.post_update , name='update'),
-    url(r'^delete/$', views.post_delete , name='delete'),
     url(r'^list/$', views.post_list , name='list'),
-	url(r'^detail/(?P<post_id>\d+)/$', views.post_detail , name='detail'),
-    ]
+    url(r'^update/(?P<post_id>\d+)/$', views.post_update , name='update'),
+    url(r'^delete/(?P<post_id>\d+)$', views.post_delete , name='delete'),
+    url(r'^detail/(?P<post_id>\d+)/$', views.post_detail , name='detail'),
+	]
